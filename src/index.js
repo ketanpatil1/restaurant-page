bfdaa12e721ffbf69cc1acc3150fc2d356d2f3a2
Ketan Patil <ketanpatil01@proton.me>
2024-10-20 13:18:41 +0530

    Create page generation files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import generateHome from "./generateHome";
+import generateMenu from "./generateMenu";
+import generateAbout from "./generateAbout";
 
-console.log(generateHome());
+const homeBtn = document.querySelector(".home-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const aboutBtn = document.querySelector(".about-btn");
+
+homeBtn.addEventListener("click", () => {
+    console.log(generateHome());
+});
+menuBtn.addEventListener("click", () => {
+    console.log(generateMenu());
+});
+aboutBtn.addEventListener("click", () => {
+    console.log(generateAbout());
+});
